c591e6d38939066c71aabb182e7e2407312a9805
ashimpaudel <ashimpaudel@gmail.com>
2019-02-02 22:28:26 -0800

    add dataset 2012 to 2017

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+var Router2017 = require('./routes/2017');
 var app = express();
 
 // view engine setup
@@ -20,7 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/2017', Router2017);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
